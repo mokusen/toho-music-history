8a1780ef9afb1db4dec39203e9628e05b420c9e5
kakin_man <kikawa.jindai@gmail.com>
2018-08-27 01:26:33 +0900

    wrapperの調整 文字サイズの調整 ヘッダー等のCSS変更 favicon対応 Button等のCSS変更

--- tohocd/forms.py
+++ tohocd/forms.py
@@ -1,33 +1,17 @@
 from django import forms
 
 class FindForm(forms.Form):
-    find = forms.CharField(max_length=255, required=False)
+    widget_setting = forms.TextInput(attrs={'class': 'search-input'})
+    find = forms.CharField(max_length=255, required=False, label="", widget=widget_setting)
 
 class DetailForm(forms.Form):
-    circle = forms.CharField(max_length=255, required=False ,widget=forms.TextInput(
-        attrs={'class': 'detail-input'}
-    ))
-    cd = forms.CharField(max_length=255, required=False ,widget=forms.TextInput(
-        attrs={'class': 'detail-input'}
-    ))
-    song = forms.CharField(max_length=255, required=False ,widget=forms.TextInput(
-        attrs={'class': 'detail-input'}
-    ))
-    vocal = forms.CharField(max_length=255, required=False ,widget=forms.TextInput(
-        attrs={'class': 'detail-input'}
-    ))
-    lyric = forms.CharField(max_length=255, required=False ,widget=forms.TextInput(
-        attrs={'class': 'detail-input'}
-    ))
-    arrange = forms.CharField(max_length=255, required=False ,widget=forms.TextInput(
-        attrs={'class': 'detail-input'}
-    ))
-    ori_work = forms.CharField(max_length=255, required=False ,widget=forms.TextInput(
-        attrs={'class': 'detail-input'}
-    ))
-    ori_song = forms.CharField(max_length=255, required=False ,widget=forms.TextInput(
-        attrs={'class': 'detail-input'}
-    ))
-    release = forms.CharField(max_length=255, required=False ,widget=forms.TextInput(
-        attrs={'class': 'detail-input'}
-    ))
+    widget_setting = forms.TextInput(attrs={'class': 'detail-input'})
+    circle = forms.CharField(max_length=255, required=False, label="" ,widget=widget_setting)
+    cd = forms.CharField(max_length=255, required=False, label="" ,widget=widget_setting)
+    song = forms.CharField(max_length=255, required=False, label="" ,widget=widget_setting)
+    vocal = forms.CharField(max_length=255, required=False, label="" ,widget=widget_setting)
+    lyric = forms.CharField(max_length=255, required=False, label="" ,widget=widget_setting)
+    arrange = forms.CharField(max_length=255, required=False, label="" ,widget=widget_setting)
+    ori_work = forms.CharField(max_length=255, required=False, label="" ,widget=widget_setting)
+    ori_song = forms.CharField(max_length=255, required=False, label="" ,widget=widget_setting)
+    release = forms.CharField(max_length=255, required=False, label="" ,widget=widget_setting)

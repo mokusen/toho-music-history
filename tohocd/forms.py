9ec4de24a454d43bf4f15ad8969b13b9e1816d83
mokusen <kikawa.tech@gmail.com>
2018-09-04 23:34:30 +0900

    formにplaceholderを追加

--- tohocd/forms.py
+++ tohocd/forms.py
@@ -6,6 +6,7 @@ class FindForm(forms.Form):
 
 class DetailForm(forms.Form):
     widget_setting = forms.TextInput(attrs={'class': 'detail-input'})
+    release_widget = forms.TextInput(attrs={'class': 'detail-input','placeholder': '2018-09-04'})
     cd = forms.CharField(max_length=255, required=False, label="CD" ,widget=widget_setting)
     circle = forms.CharField(max_length=255, required=False, label="サークル" ,widget=widget_setting)
     song = forms.CharField(max_length=255, required=False, label="曲名" ,widget=widget_setting)
@@ -14,4 +15,4 @@ class DetailForm(forms.Form):
     arrange = forms.CharField(max_length=255, required=False, label="編曲" ,widget=widget_setting)
     ori_work = forms.CharField(max_length=255, required=False, label="原作" ,widget=widget_setting)
     ori_song = forms.CharField(max_length=255, required=False, label="原曲" ,widget=widget_setting)
-    release = forms.CharField(max_length=255, required=False, label="発売日" ,widget=widget_setting)
+    release = forms.CharField(max_length=255, required=False, label="発売日" ,widget=release_widget)
